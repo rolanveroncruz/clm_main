34b6acc6b54ae44b9406a5721a55ecd5c25e5e31
rolanveroncruz <rolanveroncruz@gmail.com>
2025-06-14 13:53:50 +0800

    minor edits.

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,26 +1,11 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"net/http"
 	"ph.certs.com/clm_main/auth"
 	"strings"
 )
-
-// verifyToken validates a JWT token string and returns an error if it's invalid or cannot be parsed.
-func verifyToken(tokenString string) error {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return auth.SecretKey, nil
-	})
-	if err != nil {
-		return err
-	}
-	if !token.Valid {
-		return fmt.Errorf("invalid token")
-	}
-	return nil
-}
 
 // JWTMiddleware checks for the validity of the jwt token in the Authorization header
 // extracts the userEmail, and adds it to the request context.
@@ -33,6 +18,10 @@ func JWTMiddleware(next http.Handler) http.Handler {
 			return auth.SecretKey, nil
 		})
 		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		if !token.Valid {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
